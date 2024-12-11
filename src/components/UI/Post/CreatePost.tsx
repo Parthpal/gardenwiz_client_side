ad133360c0c5f34b4c526df06fb52571f4340990
unknown <parthapradippal@gmail.com>
2024-12-12 01:54:20 +0530

    Add console logging for all post data retrieved from form

--- src/components/UI/Post/CreatePost.tsx
+++ src/components/UI/Post/CreatePost.tsx
@@ -45,32 +45,35 @@ const CreatePost = () => {
   const { control, handleSubmit } = methods;
   const submitHandler = methods.handleSubmit;
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    const formData = new FormData();
     const postData = {
       ...data,
       content,
     };
-    console.log(postData);
+    formData.append("data", JSON.stringify(postData));
+    for (let image of imageFiles) {
+      formData.append("itemImages", image);
+    }
   };
   const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files![0];
-
     setImageFiles((prev) => [...prev, file]);
 
     if (file) {
       const reader = new FileReader();
-
       reader.onloadend = () => {
         setImagePreviews((prev) => [...prev, reader.result as string]);
       };
-
       reader.readAsDataURL(file);
     }
+    
   };
     const handleDeleteImage = (index: number) => {
-    // Remove the image preview and file at the given index
-    setImagePreviews((prev) => prev.filter((_, i) => i !== index));
-    setImageFiles((prev) => prev.filter((_, i) => i !== index));
-  };
+      // Remove the image preview and file at the given index
+      setImagePreviews((prev) => prev.filter((_, i) => i !== index));
+      setImageFiles((prev) => prev.filter((_, i) => i !== index));
+    };
+
   return (
     <>
           <Card className="max-w-7xl my-4">
@@ -97,83 +100,83 @@ const CreatePost = () => {
             </div>
             </CardBody>
             <Divider />
-    </Card>
-      <Modal
-        isOpen={isOpen}
-        size={size}
-        placement="top-center"
-        onOpenChange={onOpenChange}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Create a Post</ModalHeader>
-              <ModalBody>
-                <Card>
-                  <CardBody>
-                    <FormProvider {...methods}>
-                      <form onSubmit={submitHandler(onSubmit)}>
-                        <GWInput
-                          className=" w-full my-2"
-                          label="category"
-                          name="category"
-                        />
-                        <TextEditor content={content} setContent={setContent} />
-                        <div className="min-w-fit flex-1">
-                            <label
-                              className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400"
-                              htmlFor="image"
-                            >
-                              Upload image
-                            </label>
-                            <input
-                              multiple
-                              className="hidden"
-                              id="image"
-                              type="file"
-                              onChange={(e) => handleImageChange(e)}
+          </Card>
+          <Modal
+            isOpen={isOpen}
+            size={size}
+            placement="top-center"
+            onOpenChange={onOpenChange}
+          >
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1">Create a Post</ModalHeader>
+                  <ModalBody>
+                    <Card>
+                      <CardBody>
+                        <FormProvider {...methods}>
+                          <form onSubmit={submitHandler(onSubmit)}>
+                            <GWInput
+                              className=" w-full my-2"
+                              label="category"
+                              name="category"
                             />
-                         </div>
-                        {imagePreviews.length > 0 && (
-                          <div className="flex gap-5 my-5 flex-wrap">
-                            {imagePreviews.map((imageDataUrl, index) => (
-                              <div
-                                key={imageDataUrl}
-                                className="relative size-48 rounded-xl border-2 border-dashed border-default-300 p-2"
-                              >
-                                <img
-                                  alt="item"
-                                  className="h-full w-full object-cover object-center rounded-md"
-                                  src={imageDataUrl}
-                                />
-                                {/* Add the delete button */}
-                                <button
-                                  className="absolute top-2 right-2 bg-red-500 text-white rounded-full h-6 w-6 flex items-center justify-center text-sm"
-                                  onClick={() => handleDeleteImage(index)}
+                            <TextEditor content={content} setContent={setContent} />
+                            <div className="min-w-fit flex-1">
+                                <label
+                                  className="flex h-14 w-full cursor-pointer items-center justify-center rounded-xl border-2 border-default-200 text-default-500 shadow-sm transition-all duration-100 hover:border-default-400"
+                                  htmlFor="image"
                                 >
-                                  ✕
-                                </button>
-                              </div>
-                            ))}
-                          </div>
-                        )}
-                        <Button color="primary" className='w-full my-2' size="lg" type="submit">
-                          Post
-                        </Button>
-                      </form>
-                    </FormProvider>
-                  </CardBody>
-                </Card>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Close
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+                                  Upload image
+                                </label>
+                                <input
+                                  multiple
+                                  className="hidden"
+                                  id="image"
+                                  type="file"
+                                  onChange={(e) => handleImageChange(e)}
+                                />
+                            </div>
+                            {imagePreviews.length > 0 && (
+                            <div className="flex gap-5 my-5 flex-wrap">
+                              {imagePreviews.map((imageDataUrl, index) => (
+                                <div
+                                  key={imageDataUrl}
+                                  className="relative size-48 rounded-xl border-2 border-dashed border-default-300 p-2"
+                                >
+                                  <img
+                                    alt="item"
+                                    className="h-full w-full object-cover object-center rounded-md"
+                                    src={imageDataUrl}
+                                  />
+                                  {/* Add the delete button */}
+                                  <button
+                                    className="absolute top-2 right-2 bg-red-500 text-white rounded-full h-6 w-6 flex items-center justify-center text-sm"
+                                    onClick={() => handleDeleteImage(index)}
+                                  >
+                                    ✕
+                                  </button>
+                                </div>
+                              ))}
+                            </div>
+                          )}  
+                            <Button color="primary" className='w-full my-2' size="lg" type="submit">
+                              Post
+                            </Button>
+                          </form>
+                        </FormProvider>
+                      </CardBody>
+                    </Card>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="danger" variant="flat" onPress={onClose}>
+                      Close
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
     </>
   );
 };
